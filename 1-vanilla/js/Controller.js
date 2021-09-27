482aa30076a372d9b9dd6a576c095374ac6ac882
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-09-28 00:01:45 +0900

    Add JS

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,15 +1,17 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView }) {
+  constructor(store, { searchFormView, searchResultView, tabView }) {
     console.log(tag, "constructor");
 
     this.store = store;
 
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
+    this.tabView = tabView;
 
     this.subscribeViewEvents();
+    this.render();
   }
 
   subscribeViewEvents() {
@@ -27,18 +29,23 @@ export default class Controller {
 
   reset() {
     console.log(tag, "reset");
+
     this.store.searchKeyword = "";
     this.store.searchResult = [];
-
     this.render();
   }
 
   render() {
     if (this.store.searchKeyword.length > 0) {
-      this.searchResultView.show(this.store.searchResult);
-      return;
+      return this.renderSearchResult();
     }
 
+    this.tabView.show(this.store.selectedTab); // TODO
     this.searchResultView.hide();
+  }
+
+  renderSearchResult() {
+    this.tabView.hide();
+    this.searchResultView.show(this.store.searchResult);
   }
 }
