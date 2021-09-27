01def980857111b67c5bd3b08dcbdba9863dcf0e
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-09-27 21:24:37 +0900

    Add reset

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,9 +1,28 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, {searchFormView}) {
+  constructor(store, { searchFormView }) {
+    console.log(tag, "constructor");
+
     this.store = store;
 
     this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+  }
+
+  subscribeViewEvents() {
+    this.searchFormView.on("@submit", (event) =>
+      this.search(event.detail.value)
+    ).on("@reset", () => this.reset())
+    
+  }
+
+  search(searchKeyword) {
+    console.log(tag, "search", searchKeyword);
+  }
+
+  reset() {
+    console.log(tag, "reset");
   }
 }
