4eda768b812241b2d04a9bce9eae58f5714c9461
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-09-29 22:48:38 +0900

    Add Store

--- 1-vanilla/js/Store.js
+++ 1-vanilla/js/Store.js
@@ -1,3 +1,4 @@
+import { createNextId } from "./helpers.js";
 import { TabType } from "./views/TabView.js";
 
 const tag = "[Store]";
@@ -20,6 +21,7 @@ export default class Store {
     this.searchResult = this.storage.productData.filter((product) =>
       product.name.includes(keyword)
     );
+    this.addHistory(keyword);
   }
 
   getKeywordList() {
@@ -36,7 +38,24 @@ export default class Store {
 
   removeHistory(keyword) {
     this.storage.historyData = this.storage.historyData.filter(
-      (history) => history.keyword != keyword
-      );
+      (history) => history.keyword !== keyword
+    );
+  }
+
+  addHistory(keyword) {
+    keyword = keyword.trim()
+    if(!keyword) {
+      return;
+    }
+
+    const hasHistory = this.storage.historyData.some(history => history.keyword === keyword)
+    if(hasHistory) {
+      this.removeHistory(keyword);
+    }
+
+    const id = createNextId(this.storage.historyData)
+    const date = new Date();
+    this.storage.historyData.push({id, keyword, date})
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
   }
 }
