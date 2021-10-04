c6c03aa29a81f0aed87cc20c3e2ca97a109ebd8d
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-04 19:30:46 +0900

    Add handleChangeInput

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,20 +1,46 @@
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: "",
+    };
+  }
+
+  handleChangeInput(event) {
+    // this.state.searchKeyword =  event.target.value;
+    // this.forceUpdate();
+
+
+    //React에서 제공해주는 setState를 이용해서 값을 변경하자
+    this.setState({
+      searchKeyword: event.target.value
+    });
+
+  }
+
   render() {
     return (
       <>
-    <header>
-      <h2 className="container">검색</h2>
-    </header>
-    <div className="container">
-      <form>
-        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
-        <button type="reset" className="btn-reset"></button>
-      </form>
-    </div>
-  </>
-  );
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form>
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+              value={this.state.searchKeyword}
+              // TODO
+              onChange={event => this.handleChangeInput(event)}
+            />
+            <button type="reset" className="btn-reset"></button>
+          </form>
+        </div>
+      </>
+    );
   }
 }
 
-// TODO
 ReactDOM.render(<App />, document.querySelector("#app"));
