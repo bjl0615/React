7a6884f806ddef8d372b1d7be233f0391acd7ac2
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-04 19:50:53 +0900

    Add handleSubmit

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -7,25 +7,25 @@ class App extends React.Component {
     };
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('TODO : handleSubmit', this.state.searchKeyword)
+  }
+
   handleChangeInput(event) {
     const searchKeyword = event.target.value;
     this.setState({ searchKeyword });
   }
 
   render() {
-    // let resetButton = null;
-
-    // if(this.state.searchKeyword.length > 0 ){
-    //   resetButton = <button type="reset" className="btn-reset"></button>
-    // }
-
     return (
       <>
         <header>
           <h2 className="container">검색</h2>
         </header>
         <div className="container">
-          <form>
+          {/* TODO */}
+          <form onSubmit={event => this.handleSubmit(event)}>
             <input
               type="text"
               placeholder="검색어를 입력하세요"
@@ -33,8 +33,9 @@ class App extends React.Component {
               value={this.state.searchKeyword}
               onChange={(event) => this.handleChangeInput(event)}
             />
-            {this.state.searchKeyword.length > 0 && <button type="reset" className="btn-reset"></button>
-            }  
+            {this.state.searchKeyword.length > 0 && (
+              <button type="reset" className="btn-reset"></button>
+            )}
           </form>
         </div>
       </>
