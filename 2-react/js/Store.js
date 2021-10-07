5488747d239ec4d6971de3e40f6e564e3550019f
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-07 22:49:55 +0900

    Add React

--- 2-react/js/Store.js
+++ 2-react/js/Store.js
@@ -18,7 +18,15 @@ class Store {
   }
 
   getKeywordList() {
-    return this.storag;e.getKeywordList
+    return this.storage.keywordData;
+  }
+
+  getHistoryList() {
+    return this.storage.historyData.sort(this._sortHistory);
+  }
+
+  _sortHistory(history1, history2) {
+    return history2.date > history1.date;
   }
 }
 
