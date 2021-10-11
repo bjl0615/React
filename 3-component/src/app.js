3ef8e724dbde633551eeb950236469955cab2089
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-11 21:21:27 +0900

    Add Rest,SearchKeyword

--- 3-component/src/app.js
+++ 3-component/src/app.js
@@ -1,15 +1,26 @@
 import React from "react";
 import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
+  search(searchKeyword) {
+    console.log('TODO : search' , searchKeyword);
+  }
+  handleReset() {
+    console.log('TODO : handleReset', )
+  }
+
   render() {
-    // TODO
     return (
-    <>
-      <Header title="검색"/>
-      <Header title="프로필"/>
-      <Header title="상품"/>
-    </>
+      <>
+        <Header title="검색" />
+        <div className="container">
+          <SearchForm 
+            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            onReset={() => this.handleReset()}  
+          />
+        </div>
+      </>
     );
   }
 }
