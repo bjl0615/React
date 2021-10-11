cc29bcee5842992917ee09ae53396138598746ce
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2021-10-11 21:59:41 +0900

    Add Components

--- 3-component/src/app.js
+++ 3-component/src/app.js
@@ -3,21 +3,41 @@ import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
-  search(searchKeyword) {
-    console.log('TODO : search' , searchKeyword);
+  constructor() {
+    super();
+
+    this.state = { searchKeyword: "" };
   }
+
+  
+  handleChangeInput(searchKeyword) {
+    if(searchKeyword.length <= 0 ){
+      this.handleReset();
+    }
+
+    this.setState({ searchKeyword });
+  }
+  
+  search(searchKeyword) {
+    console.log("TODO: search", searchKeyword);
+  }
+
   handleReset() {
-    console.log('TODO : handleReset', )
+    console.log("TODO: reset");
   }
 
   render() {
+    const { searchKeyword } = this.state;
+
     return (
       <>
         <Header title="검색" />
         <div className="container">
-          <SearchForm 
+          <SearchForm
+            value={searchKeyword}
+            onChange={() => this.handleChangeInput(value)}
             onSubmit={(searchKeyword) => this.search(searchKeyword)}
-            onReset={() => this.handleReset()}  
+            onReset={() => this.handleReset()}
           />
         </div>
       </>
